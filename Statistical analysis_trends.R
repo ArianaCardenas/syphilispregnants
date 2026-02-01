a02bd8ddbcf802999413e8c2626e7326f5bc5c55
Ariana Cardenas <ariana.cardenasj@gmail.com>
2026-01-31 20:10:38 -0800

    LISA spatial analysis

--- Statistical analysis_trends.R
+++ Statistical analysis_trends.R
@@ -21,26 +21,70 @@ library(Trendy)
 # jp_model$apc   # APC por segmento con IC95%
 # jp_model$aapc  # AAPC global con IC95%
 
+#Run Rmd 5_Tendencias_general
+
 
 install.packages("segmented")
 library(segmented)
+library(ggplot2)
 
 # Ejemplo básico
 model_cong <- lm(year ~ rate, data = data_congenital)
 seg_model_cong <- segmented(model_cong, seg.Z = ~rate, npsi = 1)  # npsi = número de puntos de cambio
 
 summary(seg_model_cong)
-plot(seg_model_cong)
+plot(seg_model_cong, xlab ="Rate",)
+title("Congenital Syphilis",
+      cex.main = 1,   font.main= 1, col.main= "black",
+      adj = 0, line = 0.5)
 
 
 model_mat <- lm(year ~ rate, data = data_maternal)
 seg_model_mat<- segmented(model_mat, seg.Z = ~rate, npsi = 1)  # npsi = número de puntos de cambio
 
 summary(seg_model_mat)
-plot(seg_model_mat)
+plot(seg_model_mat, xlab ="Rate")
+title("Maternal Syphilis",
+      cex.main = 1,   font.main= 1, col.main= "black",
+      adj = 0, line = 0.5)
 
 model_screen <- lm(year ~ sypprop, data = syphilis_df)
 seg_model_screen<- segmented(model_screen, seg.Z = ~sypprop, npsi = 1)  # npsi = número de puntos de cambio
 
 summary(seg_model_screen)
-plot(seg_model_screen)
+plot(seg_model_screen, xlab ="Prop. %")
+title("Syphilis screening",
+      cex.main = 1,   font.main= 1, col.main= "black",
+      adj = 0, line = 0.5)
+
+
+install.packages("magick")
+library(magick)
+install.packages("ggplotify")
+library(ggplotify)
+
+# Replace "path/to/your/image.jpg" with your actual file path
+a <- image_read("Figures/a.png")
+a <- ggplotify::as.grob(a)
+b <- image_read("Figures/b.png")
+b <- ggplotify::as.grob(b)
+c <- image_read("Figures/c.png")
+c <- ggplotify::as.grob(c)
+# Map of the three
+
+library(gridExtra)
+library(patchwork)
+
+# Saving figure 1:
+
+figure1<-cowplot::plot_grid(a, 
+                            b, 
+                            c, 
+                            ncol = 1, labels = c("a","b", "c"), 
+                            label_size = 18)
+
+print(figure1)
+
+ggsave("Figures/Figure_Jointpoint.png", plot = figure1, width = 3.3, height = 8, dpi = 300)
+
+
--- geoda_files.R
+++ geoda_files.R
@@ -2,6 +2,8 @@
 
 #Run 6_Bivariate_maps_enaho.Rmd
 
+### CONGENITAL SYPHILIS
+
 departamentos2 = departamentos %>%
   select(c("NOMBDEP", "geometry"))
 
@@ -47,7 +49,9 @@ collapse_syph_ANC = collapse_syph_ANC %>%
 st_write(collapse_syph_ANC, "geoda/precare_cong.gpkg")
 
 
-############Maternal Syphilis
+############ MATERNAL SYPHILIS
+
+#EDUCATION
 
 collapse_syph_edu_mat = syphilis_education %>%
   select(-c("geometry")) %>%
@@ -58,4 +62,97 @@ collapse_syph_edu_mat = collapse_syph_edu_mat %>%
   left_join(departamentos2, by = "NOMBDEP")
 
 st_write(collapse_syph_edu_mat, "geoda/education_mat.gpkg")
+
+#POVERTY
+
+collapse_syph_pov_mat = syphilis_poverty %>%
+  select(-c("geometry")) %>%
+  group_by(NOMBDEP) %>%
+  summarise(across(everything(), mean), .groups = 'drop')
+
+collapse_syph_pov_mat = collapse_syph_pov_mat %>%
+  left_join(departamentos2, by = "NOMBDEP")
+
+st_write(collapse_syph_pov_mat, "geoda/poverty_mat.gpkg")
+
+#PRENATAL CARE
+
+collapse_syph_anc_mat = syphilis_precare %>%
+  select(-c("geometry")) %>%
+  group_by(NOMBDEP) %>%
+  summarise(across(everything(), mean), .groups = 'drop')
+
+collapse_syph_anc_mat = collapse_syph_anc_mat %>%
+  left_join(departamentos2, by = "NOMBDEP")
+
+st_write(collapse_syph_anc_mat, "geoda/precare_mat.gpkg")
+
+
+##### After creating the figures in GeoDa
+
+library(magick) # To import maps
+library(ggplotify) #To change the ong files to graphic objects
+
+
+###MATERNAL SYPHILIS
+
+# Replace "path/to/your/image.jpg" with your actual file path
+education_mat_plot <- image_read("Figures/GeoDa/education_mat.png")
+education_mat_plot <- ggplotify::as.ggplot(education_mat_plot)
+poverty_mat_plot <- image_read("Figures/GeoDa/poverty_mat.png")
+poverty_mat_plot <- ggplotify::as.ggplot(poverty_mat_plot)
+precare_mat_plot <- image_read("Figures/GeoDa/precare_mat.png")
+precare_mat_plot <- ggplotify::as.ggplot(precare_mat_plot)
+
+# Map of the three
+
+library(gridExtra)
+library(patchwork)
+
+# Saving figure 1:
+
+
+LISA_maternal <- (poverty_mat_plot | legend_poverty | 
+                         education_mat_plot | legend_education | 
+                         precare_mat_plot | legend_precare) + 
+  plot_layout(widths = c(3, 0.6, 3, 0.6, 3, 0.6)) + 
+  plot_annotation(
+    title = 'b. Maternal Syphilis') 
+
+
+LISA_maternal
+
+ggsave("Figures/LISA_maternal.png", 
+       plot = LISA_maternal, width = 10, height = 3, dpi = 300)
+
+
+###CONGENITAL SYPHILIS
+
+# Replace "path/to/your/image.jpg" with your actual file path
+education_cong_plot <- image_read("Figures/GeoDa/education_cong.png")
+education_cong_plot <- ggplotify::as.ggplot(education_cong_plot)
+poverty_cong_plot <- image_read("Figures/GeoDa/poverty_cong.png")
+poverty_cong_plot <- ggplotify::as.ggplot(poverty_cong_plot)
+precare_cong_plot <- image_read("Figures/GeoDa/precare_cong.png")
+precare_cong_plot <- ggplotify::as.ggplot(precare_cong_plot)
+
+# Map of the three
+# Saving figure:
+
+
+LISA_congenital <- (poverty_cong_plot | legend_poverty | 
+                    education_cong_plot | legend_education | 
+                    precare_cong_plot | legend_precare) + 
+  plot_layout(widths = c(3, 0.6, 3, 0.6, 3, 0.6)) + 
+  plot_annotation(
+    title = 'a. Congenital Syphilis') 
+
+
+LISA_congenital
+
+ggsave("Figures/LISA_congenital.png", 
+       plot = LISA_congenital, width = 10, height = 3, dpi = 300)
+
+
+
 
